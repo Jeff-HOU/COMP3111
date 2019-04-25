d900a564d6fdd58df75e9e4d9a85e9d9ec9377b0
zhangziyue <zhangziyue@10.89.155.195>
2019-04-25 11:51:04 +0800

    try3

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -90,8 +90,115 @@ public class Controller {
     "#filterCCC", "#filterNOEx", "#filterTLA"
     };
     private Vector<Section> selectedSection = new Vector<Section>();
-    AnchorPane ap = (AnchorPane)tabFilter.getContent();
     
+    
+    void selectCourse() {
+//    	if (filterCheckBox.isEmpty()) {
+//    		for (String s: filterCheckBoxName) {
+//        		filterCheckBox.put(s, false);
+//        	}
+//    	}
+    	AnchorPane ap = (AnchorPane)tabFilter.getContent();
+    	for (String s: filterCheckBoxName) {
+    		filterCheckBox.put(s, ((CheckBox) ap.lookup(s)).isSelected());
+    	}
+    	Vector<Section> selectAMPM = new Vector<Section>();
+    	if (filterCheckBox.get("#filterAM") && filterCheckBox.get("#filterPM")) {
+    		for (Course c: course) {
+    			selectAMPM.addAll(c.getSectionsThatHaveAMandPMSlots());
+    		}
+    	} else if (filterCheckBox.get("#filterAM") && !(filterCheckBox.get("#filterPM"))) {
+    		for (Course c: course) {
+    			selectAMPM.addAll(c.getSectionsThatHaveAMSlots());
+    		}
+    	} else if (!(filterCheckBox.get("#filterAM")) && filterCheckBox.get("#filterPM")) {
+    		for (Course c: course) {
+    			selectAMPM.addAll(c.getSectionsThatHavePMSlots());
+    		}
+    	} else {
+    		for (Course c: course) {
+    			for (int i = 0; i < c.getNumSections(); i++) {
+    				selectAMPM.add(c.getSection(i));
+    			}
+    		}
+    	}
+//    	System.out.println("hhssh1" + selectAMPM.size());
+    	Vector<Section> selectDay = new Vector<Section>();
+    	for (Course c: course) {
+//    		System.out.println(c.getTitle());
+    		Vector<Section> selectDayForCourse = new Vector<Section>();
+    		for (int i = 0; i < Slot.DAYS.length; i++) {
+    			if (filterCheckBox.get(filterCheckBoxName[i])) {
+//    				System.out.println(filterCheckBoxName[i] + " " + selectDayForCourse.isEmpty());
+    				if (c.getSectionsThatHaveSlotOnDay(i).isEmpty()) {
+    					selectDayForCourse.clear();
+    					break;
+    				}
+    				if (selectDayForCourse.isEmpty()) selectDayForCourse.addAll(c.getSectionsThatHaveSlotOnDay(i));
+    				else selectDayForCourse.retainAll(c.getSectionsThatHaveSlotOnDay(i));
+    			}
+    		}
+    		selectDay.addAll(selectDayForCourse);
+    	}
+//    	System.out.println("hhssh2--\\|/" + selectDay.size());
+//    	for (Section sec: selectDay) {
+//    		System.out.println(sec.getid());
+//    	}
+//    	System.out.println("hhssh2--/|\\" + selectDay.size());
+    	Vector<Section> selectCCC = new Vector<Section>();
+    	if (filterCheckBox.get("#filterCCC")) {
+    		for (Course c: course) {
+        		if (c.getcc4y()) {
+        			for (int i = 0; i < c.getNumSections(); i++) {
+        				selectCCC.add(c.getSection(i));
+        			}
+        		}
+        	}
+    	}
+    	Vector<Section> selectNOEx = new Vector<Section>();
+    	if (filterCheckBox.get("#filterNOEx")) {
+    		for (Course c: course) {
+        		if (c.getnoexclusion()) {
+        			for (int i = 0; i < c.getNumSections(); i++) {
+        				selectNOEx.add(c.getSection(i));
+        			}
+        		}
+        	}
+    	}
+    	Vector<Section> selectTLA = new Vector<Section>();
+    	if (filterCheckBox.get("#filterTLA")) {
+    		for (Course c: course) {
+        		if (c.gettla()) {
+        			for (int i = 0; i < c.getNumSections(); i++) {
+        				selectTLA.add(c.getSection(i));
+        			}
+        		}
+        	}
+    	}
+    	selectedSection.clear();
+    	selectedSection.addAll((Vector<Section>)selectAMPM.clone());// selectAMPM.clear();
+    	boolean tmp_selectday = filterCheckBox.get("#filterMON") || filterCheckBox.get("#filterTUE") || filterCheckBox.get("#filterWED") || filterCheckBox.get("#filterTHU") || filterCheckBox.get("#filterFRI");
+    	if (tmp_selectday)  selectedSection.retainAll((Vector<Section>)selectDay.clone());// selectDay.clear();
+    	if (filterCheckBox.get("#filterCCC"))  selectedSection.retainAll((Vector<Section>)selectCCC.clone());// selectCCC.clear();
+    	if (filterCheckBox.get("#filterNOEx")) selectedSection.retainAll((Vector<Section>)selectNOEx.clone());// selectNOEx.clear();
+    	if (filterCheckBox.get("#filterTLA"))  selectedSection.retainAll((Vector<Section>)selectTLA.clone());// selectTLA.clear();
+    	System.out.println("filtered course size and their id list: " + selectedSection.size());
+    	for (Section s: selectedSection) {
+    		System.out.println(s.getid());
+    	}
+//    	System.out.println("-------");
+//    	for (Section s: selectTLA) {
+//    		System.out.println(s.getid());
+//    	}
+    	textAreaConsole.setText("Filtered Courses:\n");
+    	for (Section sec: selectedSection) {
+    		textAreaConsole.appendText(sec.getCourseCode() + "\t" + sec.getcode()); // is there any extra information needed to display???
+    		textAreaConsole.appendText("\n");
+    		for (int i = 0; i < sec.getNumSlots(); i++) {
+    			textAreaConsole.appendText("\t" + sec.getSlot(i) + "\n");
+    		}
+    	}
+    }
     
     
     
