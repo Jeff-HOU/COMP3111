1672e898866dd7b127438d1005ebed6c1cd9e3d0
zhangziyue <zhangziyue@10.89.155.195>
2019-04-25 12:22:10 +0800

    NewFile

---
+++ src/main/java/comp3111/coursescraper/TermNotValidError.java
@@ -0,0 +1,8 @@
+package comp3111.coursescraper;
+
+public class TermNotValidError {
+	private static final long serialVersionUID = 1L;
+
+ 	public TermNotValidError(String errorMessage) {
+        super(errorMessage);
+}
